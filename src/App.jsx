3bcc5412c1cb688d300630596594c895937a3a3e
haitamjebari <haitamjebari02@gmail.com>
2026-02-11 14:10:41 +0100

    add routing and login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Navbar from "./components/mainNavbar.jsx";
 import Songs from "./components/songs.jsx";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Login from "./components/login/login.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-  </>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+  // <App/>
 );
